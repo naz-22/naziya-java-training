0ab8b392c7dbb097f271dc54789b12a0e655026d
azuser <azuser@jav1.5kxnye1v1mdujcfnd5cz4jfn3a.rx.internal.cloudapp.net>
2023-09-07 07:16:58 +0000

    7th commit

---
+++ Day9date&time/src/Empexperience.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.time.*;
+import java.time.LocalDate;
+import java.time.Period;
+public class Empexperience {
+
+	public static void main(String[] args) {
+		
+LocalDate joiningdate=LocalDate.of(2022, 7 ,21);
+LocalDate relievingdate=LocalDate.of(2024, 7 ,22);
+
+//Period diff = Period.between(joiningdate, relievingdate);
+//
+//System.out.printf("Difference is %d years, %d months and %d days old",
+//					diff.getYears(), diff.getMonths(), diff.getDays());
+
+
+
+
+
+//Period diff = Period.between(joiningdate, relievingdate);
+//
+//System.out.printf("Difference is %d years, %d months and %d days old",
+//					diff.getYears(), diff.getMonths(), diff.getDays());
+	}
+
+}
